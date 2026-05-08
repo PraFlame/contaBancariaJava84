5cf1d19f30f769233a069efaceaec12a2bc836c4
Flame <parasouzaf@gmail.com>
2026-05-08 17:58:41 -0300

    Menu finalizado

--- src/contaBancaria/Menu.java
+++ src/contaBancaria/Menu.java
@@ -157,14 +157,23 @@ public class Menu {
 				break;
 			case 6:
 				System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
+				
+				sacar();
+				
 				keyPress();
 				break;
 			case 7:
 				System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
+				
+				depositar();
+				
 				keyPress();
 				break;
 			case 8:
 				System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
+				
+				transferir();
+				
 				keyPress();
 				break;
 			default:
@@ -312,6 +321,42 @@ public class Menu {
 		else {
 			System.out.println("\nOperação cancelada.");
 		}
+	}
+	
+	private static void sacar() {
+		
+		System.out.println("Digite o número da conta:");
+		int numero = leia.nextInt();
+		
+		System.out.println("Digite o valor do saque:");
+		float valor = leia.nextFloat();
+		
+		contaController.sacar(numero, valor);
+	}
+	
+	private static void depositar() {
+		
+		System.out.println("Digite o número da conta:");
+		int numero = leia.nextInt();
+		
+		System.out.println("Digite o valor do depósito:");
+		float valor = leia.nextFloat();
+		
+		contaController.depositar(numero, valor);
+	}
+	
+	private static void transferir() {
+		
+		System.out.println("Digite o número da conta de Origem:");
+		int numeroOrigem = leia.nextInt();
+		
+		System.out.println("Digite o número da conta de Destino:");
+		int numeroDestino = leia.nextInt();
+		
+		System.out.println("Digite o valor da transferência:");
+		float valor = leia.nextFloat();
+		
+		contaController.transferir(numeroOrigem, numeroDestino, valor);
 	}
 	
 
--- src/contaBancaria/controller/ContaController.java
+++ src/contaBancaria/controller/ContaController.java
@@ -67,18 +67,48 @@ public class ContaController implements ContaRepository {
 	}
 	@Override
 	public void sacar(int numero, float valor) {
-		// TODO Auto-generated method stub
+		var conta = buscarNaCollection(numero);
 		
+				if(conta != null) {
+					if(conta.sacar(valor) == true) {
+						System.out.printf("\nO Saque na Conta: %d foi efetuado com sucesso!", numero);
+					}
+				}
+				else {
+					System.out.printf("A conta número: %d não foi encontrada!%n", numero);					
+				}
 	}
 	@Override
 	public void depositar(int numero, float valor) {
-		// TODO Auto-generated method stub
+		var conta = buscarNaCollection(numero);
+		
+		if(conta != null) {
+			conta.depositar(valor);
+			System.out.printf("\nO Depósito na Conta: %d foi efetuado com sucesso!", numero);
+		}
+		else {
+			System.out.printf("A conta número: %d não foi encontrada!%n", numero);					
+		}
 		
 	}
 	@Override
 	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
-		// TODO Auto-generated method stub
 		
+		var contaOrigem = buscarNaCollection(numeroOrigem);
+		var contaDestino = buscarNaCollection(numeroDestino);
+		
+		if(contaOrigem != null && contaDestino != null) {
+			
+			if(contaOrigem.sacar(valor) == true) {
+				contaDestino.depositar(valor);
+				System.out.printf(
+						"\nA Transferência da conta: %d, para a conta: %d foi efetuada com sucesso!",
+						numeroOrigem, numeroDestino);
+			}
+		}
+		else {
+			System.out.println("\nA Conta Origem e/ou Destino não foram encontradas!");
+		}
 	}
 	
 	
